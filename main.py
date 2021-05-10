1c3bca703d4bd7cb3c150c93a776675ef9544499
LiTiBe <contact@litiboost.fr>
2021-05-10 10:22:56 +0200

    création du fichier main.py principal pour la création du scripts d'extraction. Mise en place du Venv.

---
+++ main.py
@@ -0,0 +1,24 @@
+# import beautifulsoup pour scapping page
+# import module creation fichier csv
+import os
+
+#import module depuis dossier bs_script
+
+#pseudo code pour le script final
+
+def main() :
+    """
+    url_site_to_scap = http://books.toscrape.com/
+
+    categories = extractCategoriesFromUrl()
+    for categorie in categories :
+        extractUrlProductsFromCategorie()
+        for urlproduct in urlProducts :
+            dicoProduct = extractInfosFromUrlProduct()
+            creationCSVCategorie
+    """
+
+if __name__ == "__main__":
+    main()
+else :
+    print("Merci d'exécuter le fichier main.py directement")
